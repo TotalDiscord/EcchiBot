f3673d123f87b6a90fc5e95b82620657e9787132
cikeZ00 <17727672+cikeZ00@users.noreply.github.com>
2020-02-15 04:01:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ async def help(ctx):
     helpembed = discord.Embed(color=discord.Color.red())
     helpembed.set_author(name="Help (contact cikeZ00#5068 for help)")
     helpembed.add_field(name="anime", value="Sends SFW anime images.",inline=False)
-    helpembed.add_field(name="booru", value="Usage: ``booru (tag)``, sends an image according to the tag you specifiy.",inline=False)
+    helpembed.add_field(name="booru", value="Usage: ``booru (tag)``, sends an image according to the tag(s) you specifiy.[2 MAX]",inline=False)
     helpembed.add_field(name="hentai", value="Sends hentai images.",inline=False)
     helpembed.add_field(name="ping", value="Plays ping pong",inline=False)
     helpembed.add_field(name="help", value="Shows help.",inline=False)
